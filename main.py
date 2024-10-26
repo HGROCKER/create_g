61fec3d0e443588f19b3ae9a87665694e7b2b2da
HGROCKER <135489210+HGROCKER@users.noreply.github.com>
2024-10-26 20:48:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame,sys,random,time,math
+import pygame,sys,random,math
 pygame.init()
 
 class color:
@@ -14,7 +14,7 @@ class main_game:
         self.play =False
         self.FPS = pygame.time.Clock()
         self.num_FPS = 70
-        self.time_create_obj = 1000
+        self.time_create_obj = 500
 game = main_game()
 
 class main_object:
@@ -24,14 +24,15 @@ class main_object:
         self.color = c
         self.radius =r
 
-m_obj= main_object(game.width*0.5,game.height*0.5,0.2*game.height,color.black)
+m_obj= main_object(game.width*0.5,game.height*0.5,0.1*game.width,color.red)
 
 class object_c:
     def __init__(self,center,w,s,di):
         self.w= w 
         self.center= center
-        self.color = color.black
+        self.color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))#color.black
         self.speed = s
+
 
         l_x = di[0]-center[0]
         l_y = di[1]-center[1]
@@ -39,9 +40,9 @@ class object_c:
         l_x /= len
         l_y /= len
         self.direction = (l_x,l_y)
+
     def move(self):        
         self.center=(self.center[0] +self.direction[0],self.center[1] +self.direction[1])
-
 
 
         
@@ -80,10 +81,11 @@ dk_de = True
 while True:
     game.play=pause(game.play)
 
-    if pygame.time.get_ticks() % game.time_create_obj <70 and dk_de:
-        list_object.append(object_c(center_spon(), random.randint(game.width*0.1,game.width*0.2),0.01*game.width, (m_obj.x,m_obj.y)))
+    if pygame.time.get_ticks() % game.time_create_obj <50 and dk_de:
+        list_object.append( object_c(center_spon(), random.randint(game.width*0.05,game.width*0.07), 0.01*game.width , (m_obj.x,m_obj.y)) )
+        game.time_create_obj = random.randint(200,5000)
         dk_de = False
-    elif pygame.time.get_ticks() % game.time_create_obj >70 and dk_de==False:
+    elif pygame.time.get_ticks() % game.time_create_obj >50 and dk_de==False:
         dk_de=True
 
     for event in pygame.event.get():
@@ -97,11 +99,14 @@ while True:
             if get_k == ord(' '):
                 game.play = True
                 print("pause")
+        elif get == pygame.MOUSEBUTTONDOWN:
+            pass
     view.fill(game.color)
 
     for obj in list_object:
         pygame.draw.circle(view,obj.color,obj.center,obj.w)
         obj.move()
+    pygame.draw.circle(view,m_obj.color,(m_obj.x,m_obj.y),m_obj.radius)
     print(len(list_object))
     game.FPS.tick(game.num_FPS)
     pygame.display.update()
