155fc3e226e133b65b0416161d213e4d7d7a9606
HGROCKER <135489210+HGROCKER@users.noreply.github.com>
2024-10-26 17:45:06 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame,sys,random
+import pygame,sys,random,time
 pygame.init()
 
 class color:
@@ -16,23 +16,27 @@ class main_game:
 game = main_game()
 
 class main_object:
-    def __init__(self,x,y,r):
+    def __init__(self,x,y,r,c):
         self.x =x
         self.y =y
-        self.color = color.black
+        self.color = c
         self.radius =r
-m_obj= main_object(game.width*0.5,game.height*0.5,0.2*game.height)
+
+m_obj= main_object(game.width*0.5,game.height*0.5,0.2*game.height,color.black)
 
 class object_c:
-    def __init__(self,x,y,w,s):
+    def __init__(self,x,y,w,s,di):
         self.w= w 
         self.x = x
         self.y = y
         self.color = color.black
         self.speed = s
+        self.direction = di
     def move(self):
         pass
 list_object = []
+list_object.append(object_c(random.randint(0,game.width), random.randint(0,game.height), random.randint(game.width*0.1,game.width*0.2),0.1*game.width, (m_obj.x,m_obj.y)))
+
 view = pygame.display.set_mode((game.width,game.height))
 
 
@@ -41,6 +45,8 @@ t_pause = font.render("PAUSE",True,color.black)
  
 def pause(key):
     while key:
+        list_object.append(object_c(random.randint(0,game.width), random.randint(0,game.height), random.randint(game.width*0.1,game.width*0.2),0.1*game.width, (m_obj.x,m_obj.y)))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT :
                 pygame.quit()
@@ -54,6 +60,8 @@ def pause(key):
 
 while True:
     game.play=pause(game.play)
+    # list_object.append(object_c(random.randint(0,game.width), random.randint(0,game.height), random.randint(game.width*0.1,game.width*0.2),0.1*game.width, (m_obj.x,m_obj.y)))
+
     for event in pygame.event.get():
         get = event.type
         if get == pygame.QUIT :
@@ -66,4 +74,6 @@ while True:
                 game.play = True
                 print("pause")
     view.fill(game.color)
+    # for obj in list_object:
+    #     view.blit(pygame.draw.circle(obj.))
     pygame.display.update()
