fb7b5b6d51e2419c43195a560494b9b1fdd9c7f3
HGROCKER <135489210+HGROCKER@users.noreply.github.com>
2024-10-26 14:50:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame,sys,random
 pygame.init()
 
+class color:
+    red =(255,0,0)
+    black = (0,0,0)
+    white= (255,255,255)
+
 class main_game:
     def __init__(self):
         self.width = 500
         self.height =800
-        self.color = (255,255,255)
+        self.color = color.white
         self.play =False
         self.time = pygame.time.Clock()
 game = main_game()
@@ -14,7 +19,7 @@ class main_object:
     def __init__(self,x,y,r):
         self.x =x
         self.y =y
-        self.color = (0,0,0)
+        self.color = color.black
         self.radius =r
 m_obj= main_object(game.width*0.5,game.height*0.5,0.2*game.height)
 
@@ -23,16 +28,19 @@ class object_c:
         self.w= w 
         self.x = x
         self.y = y
-        self.color = (0,0,0)
+        self.color = color.black
         self.speed = s
     def move(self):
         pass
 list_object = []
 view = pygame.display.set_mode((game.width,game.height))
 
+
+font = pygame.font.Font('freesansbold.ttf', 32)
+t_pause = font.render("PAUSE",True,color.black)
+ 
 def pause(key):
     while key:
-        game.time.tick(20)
         for event in pygame.event.get():
             if event.type == pygame.QUIT :
                 pygame.quit()
@@ -40,6 +48,10 @@ def pause(key):
             elif event.type == pygame.KEYDOWN:
                 print("continue")
                 return False
+        view.blit(t_pause,(0,0))
+        pygame.display.update()
+        game.time.tick(20)
+
 while True:
     game.play=pause(game.play)
     for event in pygame.event.get():
