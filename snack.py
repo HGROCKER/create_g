7e082b255b9cb92c62aa9bbbb5369e898ae9de19
HGROCKER <135489210+HGROCKER@users.noreply.github.com>
2024-10-28 10:51:25 +0700

    Update snack.py

--- snack.py
+++ snack.py
@@ -1,89 +1,116 @@
-import pygame
-import sys
-import math,random
+import pygame,random
+
 # Khởi tạo Pygame
 pygame.init()
 
-# Kích thước màn hình
-screen_width = 800
-screen_height = 600
+# Thiết lập màn hình
+screen_width = 600
+screen_height = 400
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Rắn săn mồi")
 
 # Màu sắc
 white = (255, 255, 255)
-blue = (0, 0, 255)
+black = (0, 0, 0)
 red = (255, 0, 0)
 
-# Đối tượng main
-d=0
-rect_x = 50
-rect_y = 50
-rect_width = 50
-rect_height = 50
-xspeed = 5
-player_rect = pygame.Rect(rect_x, rect_y, rect_width, rect_height)
-font = pygame.font.Font('freesansbold.ttf', 32)
-stop=True
+# Khởi tạo con rắn
+snake_block = 10
+snake_speed = 15
+font_style = pygame.font.SysFont(None, 25)
 
-# Tọa độ mục tiêu
-rect2_x =400
-rect2_y =300
-rect2_width = 50
-rect2_height = 50
-target_rect = pygame.Rect(rect2_x, rect2_y, rect2_width, rect2_height)
-end= False
+def Your_score(score):
+    value = font_style.render("Your Score: " + str(score), True, black)
+    screen.blit(value, [0, 0])
 
-# Vòng lặp chính
-pause=True
-while True:
-    t_pause = font.render("DIEM:{0}-({1};{2})".format(d,player_rect.x,player_rect.y),True,(0,0,0))
-    pausegame = font.render("PAUSE",True,(0,0,0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()        
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_p:
-                screen.blit(pausegame,(600,0))
-                pygame.display.flip()
-                pause= False
-            if event.key == pygame.K_r:
-                pause = True
-    if pause==False: 
-        pass
-    if pause==True:
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] or keys[ord('a')]:
-            player_rect.x -= xspeed
-          
-        if keys[pygame.K_RIGHT] or keys[ord('d')]:
-            player_rect.x += xspeed
-        if keys[pygame.K_UP] or keys[ord('w')]:
-            player_rect.y -= xspeed
-        if keys[pygame.K_DOWN] or keys[ord('s')]:
-            player_rect.y += xspeed
-        if player_rect.x <-50 :
-            player_rect = pygame.Rect(player_rect.x + 900, player_rect.y, rect_width, rect_height)
-        elif player_rect.x >850 :
-            player_rect = pygame.Rect(player_rect.x - 900, player_rect.y, rect_width, rect_height)
-        if player_rect.y <-50:
-            player_rect = pygame.Rect(player_rect.x, player_rect.y + 700, rect_width, rect_height)
-        elif player_rect.y >650:
-            player_rect = pygame.Rect(player_rect.x, player_rect.y - 700, rect_width, rect_height)
-    # Kiểm tra va chạm
-        if player_rect.colliderect(target_rect):
-            d=d+1
-            if xspeed<10:
-                xspeed+=0.1
-            rect2_x=random.randint(25,775)
-            rect2_y=random.randint(25,575)
-            target_rect = pygame.Rect(rect2_x, rect2_y, 50, 50)
-    # Vlại màn hình
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, black, [x[0], x[1], snake_block, snake_block])
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    screen.blit(mesg, [screen_width/6, screen_height/3])
+
+# Vòng lặp game
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = screen_width/2
+    y1 = screen_height/2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
+
+    while not game_over:
+
+        while game_close == True:
+            screen.fill(white)
+            message("You Lost! Press Q-Quit or C-Play Again", red)
+            Your_score(Length_of_snake - 1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT and x1_change!=snake_block:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT and x1_change!=-snake_block:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP and y1_change!=snake_block:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN and y1_change !=-snake_block:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= screen_width or x1 < 0 or y1 >= screen_height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
         screen.fill(white)
-        pygame.draw.rect(screen, blue, player_rect)  # Đối tượng
-        pygame.draw.rect(screen, red, target_rect)    # Mục tiêu
-        screen.blit(t_pause,(50,0))
-        pygame.display.flip()
-    # Giới hạn tốc độ khung hình
-    pygame.time.Clock().tick(60)
+        pygame.draw.rect(screen, red, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
 
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+
+        pygame.time.Clock().tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+gameLoop()
